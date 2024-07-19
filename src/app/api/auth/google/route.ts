f0a6e05ced678fc4763fa778c49377179e6e7b5d
LinQuixote <leein9404@naver.com>
2024-07-19 16:16:31 +0900

    add: 구글 로그인 진행 중

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -1,6 +1,10 @@
+import { NextResponse } from "next/server"
 import { createClient } from "@/supabase/server"
 
-const supabase = createClient()
-supabase.auth.signInWithOAuth({
-  provider: "google",
-})
+export async function POST() {
+  const supabase = createClient()
+  supabase.auth.signInWithOAuth({
+    provider: "google",
+  })
+  return NextResponse.json("")
+}
