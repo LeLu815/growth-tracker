5065081e8ea5b415c5b7b8588c2be98a2de7183a
LinQuixote <leein9404@naver.com>
2024-07-19 16:49:19 +0900

    add: 구글 로그인 진행 중

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -1,10 +1,16 @@
-import { NextResponse } from "next/server"
+import { redirect } from "next/navigation"
 import { createClient } from "@/supabase/server"
 
-export async function POST() {
+export async function GET() {
   const supabase = createClient()
-  supabase.auth.signInWithOAuth({
+  const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
+    options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/callback`,
+    },
   })
-  return NextResponse.json("")
+
+  if (data.url) {
+    redirect(data.url) // use the redirect API for your server framework
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+"use client"
+
+import axios from "axios"
+
 export default function Home() {
-  return <div>배포하겠음 하이</div>;
+  const handleClick = async () => {
+    await axios.get("/api/auth/google")
+  }
+  return (
+    <div>
+      <div>배포하겠음 하이</div>
+      <button
+        onClick={handleClick}
+        className="border border-cyan-500 bg-white px-4 py-2 text-cyan-500 hover:brightness-95 active:brightness-75"
+      >
+        구글 로그인
+      </button>
+    </div>
+  )
 }
