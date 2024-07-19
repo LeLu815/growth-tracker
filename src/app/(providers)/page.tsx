ab0713cd0dd0527d3274b8175ff842ffacf30d7c
LinQuixote <leein9404@naver.com>
2024-07-19 21:33:00 +0900

    feat: 소셜, 이메일 로그인 기능구현, 전역프로바이더 설정

--- src/app/(providers)/page.tsx
+++ src/app/(providers)/page.tsx
@@ -1,55 +1,5 @@
 "use client"
 
-import { getGoogleLogin, getKaKaoLogin, getLogout, getMe } from "@/api/auth/google/api.google"
-import { useEffect, useState } from "react"
-
 export default function Home() {
-  const [user, setUser] = useState<string>("")
-
-  const handleClickGoogleLogin = async () => {
-    await getGoogleLogin()
-  }
-  const handleClickKakaoLogin = async () => {
-    await getKaKaoLogin()
-  }
-  const handleClickLogOut = async () => {
-    const error = await getLogout()
-    if (!error) {
-      setUser("")
-    }
-  }
-  const GetUserData = async () => {
-    const data = await getMe()
-    return data
-  }
-
-  useEffect(() => {
-    GetUserData().then((r) => setUser(r ? r : ""))
-  }, [])
-  return (
-    <div>
-      <div>현재 로그인 유저</div>
-      <p>{user !== "" ? user : "없습니다."}</p>
-      <div className="flex gap-5">
-        <button
-          onClick={handleClickGoogleLogin}
-          className="border border-cyan-500 bg-white px-4 py-2 text-cyan-500 hover:brightness-95 active:brightness-75"
-        >
-          구글 로그인
-        </button>
-        <button
-          onClick={handleClickKakaoLogin}
-          className="border border-cyan-500 bg-white px-4 py-2 text-cyan-500 hover:brightness-95 active:brightness-75"
-        >
-          카카오 로그인
-        </button>
-        <button
-          onClick={handleClickLogOut}
-          className="border border-cyan-500 bg-white px-4 py-2 text-cyan-500 hover:brightness-95 active:brightness-75"
-        >
-          로그아웃
-        </button>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
