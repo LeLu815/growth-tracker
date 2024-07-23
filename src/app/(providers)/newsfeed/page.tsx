da05cc1b79a42e8412c2cfd027b6b2f7c8c256c4
minyoungKuk <75466519+minyoungKuk@users.noreply.github.com>
2024-07-24 00:06:48 +0900

    feat: modal component, context 설정 완료

--- src/app/(providers)/newsfeed/page.tsx
+++ src/app/(providers)/newsfeed/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import { useRouter } from "next/navigation"
+import { useModal } from "@/context/modal.context"
 import { useQuery } from "@tanstack/react-query"
 
 import Page from "@/components/Page"
@@ -18,6 +19,23 @@ function NewsfeedPage() {
   const [userId, setUserId] = useState<string>("")
   const [category, setCategory] = useState<string>("전체보기")
   const [searchQuery, setSearchQuery] = useState<string>("")
+
+  const modal = useModal()
+
+  const handleOpenAlertModal = () => {
+    modal.open({
+      type: "alert",
+      content: "알림 모달",
+    })
+  }
+
+  const handleOpenConfirmModal = () => {
+    modal.open({
+      type: "confirm",
+      content: "이건 확인 모달",
+      onConfirm: () => console.log("확인 버튼 클릭 시 적용될 로직 넣어주세요"),
+    })
+  }
 
   const router = useRouter()
 
@@ -60,6 +78,9 @@ function NewsfeedPage() {
         category={category}
         onSelectCategory={handleCategoryClick}
       />
+
+      <button onClick={handleOpenAlertModal}>알림 모달 열기</button>
+      <button onClick={handleOpenConfirmModal}>확인 모달 열기</button>
 
       {/* 검색 필터 */}
       <SearchFilter onSearch={handleSearch} />
