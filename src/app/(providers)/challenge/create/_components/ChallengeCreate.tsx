85631c933aa4431a571d1a13c813fc3e94b3c7f3
LinQuixote <leein9404@naver.com>
2024-07-24 16:05:58 +0900

    feat: 데이터 업데이트 구현 중

--- src/app/(providers)/challenge/create/_components/ChallengeCreate.tsx
+++ src/app/(providers)/challenge/create/_components/ChallengeCreate.tsx
@@ -1,20 +1,31 @@
 "use client"
 
 import { useState } from "react"
+import { POSTchallengeArgumentType } from "@/api/supabase/challenge"
 import { useAuth } from "@/context/auth.context"
-import { useChallengeQuery } from "@/query/challenge/userChallengeQuery"
+import useChallengeQuery from "@/query/challenge/userChallengeQuery"
 
 function ChallengeCreate() {
   // 챌린지 관련 데이터
-  const [title, setTitle] = useState<string>("")
-  const [startDate, setStartDate] = useState<string>("")
-  const [endDate, setEndDate] = useState<string>("")
-  // 마일스톤 관련 데이터
-  const [routineCount, setRoutineCount] = useState<number>(0)
-  // 루틴 관련 데이터
+  const [goal, setGoal] = useState<string>("")
+  const [start_at, setStart_at] = useState<string>("")
+  const [end_at, setEnd_at] = useState<string>("")
+  const [is_secret, setIs_secret] = useState<false>(false)
+  const [day_cnt, setDay_cnt] = useState<number>(0)
+  const [user_id, setUser_id] = useState<string>("")
+  const [state, setState] = useState<"" | "" | "">("")
+  const [catetegory, setCatetegory] = useState<"" | "">("")
 
-  const { challengeCreateMutate, challengeCreateIsPending } =
-    useChallengeQuery()
+  // 마일스톤 객체 리스트
+  const [milestones, setMilestones] = useState<
+    POSTchallengeArgumentType["milestone"]
+  >([])
+
+  const {
+    challengeCreateMutate,
+    challengeCreateIsPending,
+    challengeUpdateMutate,
+  } = useChallengeQuery()
 
   // auth 데이터
   const { me } = useAuth()
@@ -34,57 +45,117 @@ function ChallengeCreate() {
         challenge_id: "",
         end_at: "2024-7-24",
         start_at: "2024-7-23",
-        success_requirement_cnt: 30,
-        total_cnt: 40,
-        total_day: 30,
+        success_requirement_cnt: 10,
+        total_cnt: 10,
+        total_day: 10,
         is_fri: true,
+        is_mon: true,
         is_sat: true,
+        is_sun: true,
+        is_thu: true,
+        is_tue: true,
+        is_wed: false,
+      },
+      {
+        challenge_id: "",
+        end_at: "2024-7-24",
+        start_at: "2024-7-23",
+        success_requirement_cnt: 20,
+        total_cnt: 20,
+        total_day: 20,
+        is_fri: true,
+        is_mon: true,
+        is_sat: true,
+        is_sun: true,
+        is_thu: true,
+        is_tue: false,
+        is_wed: false,
       },
       {
         challenge_id: "",
         end_at: "2024-7-24",
         start_at: "2024-7-23",
         success_requirement_cnt: 30,
-        total_cnt: 40,
+        total_cnt: 30,
         total_day: 30,
         is_fri: true,
+        is_mon: true,
         is_sat: true,
+        is_sun: true,
+        is_thu: false,
+        is_tue: false,
+        is_wed: false,
       },
       {
         challenge_id: "",
         end_at: "2024-7-24",
         start_at: "2024-7-23",
-        success_requirement_cnt: 30,
+        success_requirement_cnt: 40,
         total_cnt: 40,
-        total_day: 30,
+        total_day: 40,
         is_fri: true,
+        is_mon: true,
         is_sat: true,
-      },
-      {
-        challenge_id: "",
-        end_at: "2024-7-24",
-        start_at: "2024-7-23",
-        success_requirement_cnt: 30,
-        total_cnt: 40,
-        total_day: 30,
-        is_fri: true,
-        is_sat: true,
-      },
-      {
-        challenge_id: "",
-        end_at: "2024-7-24",
-        start_at: "2024-7-23",
-        success_requirement_cnt: 30,
-        total_cnt: 40,
-        total_day: 30,
-        is_fri: true,
-        is_sat: true,
+        is_sun: false,
+        is_thu: false,
+        is_tue: false,
+        is_wed: false,
       },
     ],
     routine: [
       [
         {
-          content: "루틴 생성 테스트 이인",
+          content: "10 - 루틴 생성 테스트 이인 ddlsmaa",
+          milestone_id: "",
+        },
+        {
+          content: "10 - 루틴 생성 테스트 이인 ef;ka;sc",
+          milestone_id: "",
+        },
+        {
+          content: "10 - 루틴 생성 테스트 이인 12r1f4tfe",
+          milestone_id: "",
+        },
+      ],
+      [
+        {
+          content: "20 - 루틴 생성 테스트 이인 ddlsmaa",
+          milestone_id: "",
+        },
+        {
+          content: "20 - 루틴 생성 테스트 이인 ef;ka;sc",
+          milestone_id: "",
+        },
+        {
+          content: "20 - 루틴 생성 테스트 이인 12r1f4tfe",
+          milestone_id: "",
+        },
+      ],
+      [
+        {
+          content: "30 - 루틴 생성 테스트 이인 ddlsmaa",
+          milestone_id: "",
+        },
+        {
+          content: "30 - 루틴 생성 테스트 이인 ef;ka;sc",
+          milestone_id: "",
+        },
+        {
+          content: "30 - 루틴 생성 테스트 이인 12r1f4tfe",
+          milestone_id: "",
+        },
+      ],
+      [
+        {
+          content: "40 - 루틴 생성 테스트 이인 ddlsmaa",
+          milestone_id: "",
+        },
+        {
+          content: "40 - 루틴 생성 테스트 이인 ef;ka;sc",
+          milestone_id: "",
+        },
+        {
+          content: "40 - 루틴 생성 테스트 이인 12r1f4tfe",
           milestone_id: "",
         },
       ],
@@ -96,16 +167,16 @@ function ChallengeCreate() {
       <input
         type="text"
         placeholder="챌린지 이름"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        value={goal}
+        onChange={(e) => setGoal(e.target.value)}
       />
       <div>
         <label htmlFor="">챌린지 시작날짜</label>
         <input
           className="text-black"
           type="date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
+          value={start_at}
+          onChange={(e) => setStart_at(e.target.value)}
         />
       </div>
       <div>
@@ -113,8 +184,8 @@ function ChallengeCreate() {
         <input
           className="text-black"
           type="date"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
+          value={end_at}
+          onChange={(e) => setEnd_at(e.target.value)}
         />
       </div>
 
@@ -125,8 +196,8 @@ function ChallengeCreate() {
         <input
           className="text-black"
           type="date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
+          value={start_at}
+          onChange={(e) => setStart_at(e.target.value)}
         />
       </div>
       <div>
@@ -134,13 +205,26 @@ function ChallengeCreate() {
         <input
           className="text-black"
           type="date"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
+          value={end_at}
+          onChange={(e) => setEnd_at(e.target.value)}
         />
       </div>
 
       <button onClick={() => challengeCreateMutate(INITIAL_DATA)}>
         챌린지 생성하기 버튼
+      </button>
+      <button
+        onClick={() =>
+          challengeUpdateMutate({
+            "challenge-id": "663d4d24-0d90-4f8e-b5fc-0796d0e9ba5f",
+            milestoneIds: ["b5e93309-8b13-4e93-acc9-1dcd5a1c2f27"],
+            milestone: INITIAL_DATA.milestone,
+            routine: INITIAL_DATA.routine,
+          })
+        }
+        className="mt-9 flex items-center justify-center rounded border border-white bg-neutral-600 px-4 py-2 hover:brightness-90 active:brightness-75"
+      >
+        챌린지 업데이트 버튼
       </button>
     </div>
   )
