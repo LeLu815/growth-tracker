a431ff165b5cd8fa1e67aab2d0b764b8d2462880
LinQuixote <leein9404@naver.com>
2024-07-25 21:34:21 +0900

    fix: 날짜계산 버그 수정

--- src/app/(providers)/(styles)/challenge/create/calender/calender.tsx
+++ src/app/(providers)/(styles)/challenge/create/calender/calender.tsx
@@ -1,81 +1,30 @@
 "use client"
 
-import { useState } from "react"
-import {
-  addDays,
-  differenceInCalendarDays,
-  eachDayOfInterval,
-  format,
-  getDay,
-  isBefore,
-  startOfToday,
-} from "date-fns"
+import { createContext, useState } from "react"
+import { addDays, isBefore, startOfToday } from "date-fns"
 import { DateRange } from "react-day-picker"
 
 import { Calendar } from "@/components/ui/calendar"
 
 import SelectWeek from "./selectWeek"
 
-const PERCENT_FOR_SUCCESS = "50"
-
 function Calender() {
-  const WEEK_DAY_LIST = ["월", "화", "수", "목", "금", "토", "일"]
-  const defaultValue = WEEK_DAY_LIST.map(() => false)
-
   const defaultMonth = new Date()
   const defaultSelected: DateRange = {
     from: defaultMonth,
     to: addDays(defaultMonth, 7),
   }
-  const [dayChecks, setDayChecks] = useState<boolean[]>(defaultValue)
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
-
   const today = startOfToday()
 
-  const formatDate = (date: Date | undefined) => {
-    return date ? format(date, "yyyy-MM-dd") : "날짜 없음"
-  }
-  const calculateTotalDays = (range: DateRange | undefined) => {
-    if (range?.from && range?.to) {
-      return differenceInCalendarDays(range.to, range.from) + 1
-    }
-    return 0
-  }
-  const calculateSpecificWeekdays = (
-    range: DateRange | undefined,
-    weekdays: number[]
-  ) => {
-    if (range?.from && range?.to) {
-      const allDays = eachDayOfInterval({ start: range.from, end: range.to })
-      return allDays.filter((day) => weekdays.includes(getDay(day))).length
-    }
-    return 0
-  }
-  // 1: 월요일, 2: 화요일, 3: 수요일, 4: 목요일, 5: 금요일, 6: 토요일, 7: 일요일
-  const totalMondaysAndWednesdays = calculateSpecificWeekdays(
+  const contextValue = {
     range,
-    dayChecks
-      .map((dayCheck, index) => {
-        if (dayCheck) {
-          return index + 1
-        }
-        return undefined
-      })
-      .filter((value): value is number => value !== undefined)
-  )
-
+  }
   return (
     <>
-      <SelectWeek getDayCheckList={setDayChecks} />
-      <div>
-        <p>시작 : {formatDate(range?.from)}</p>
-        <p>끝 : {formatDate(range?.to)}</p>
-        <p>총 기간 : {calculateTotalDays(range)}일</p>
-        <p>총 횟수 : {totalMondaysAndWednesdays}번</p>
-        <p>
-          성공을 위한 최소 횟수 : {Math.ceil(totalMondaysAndWednesdays / 2)}회
-        </p>
-      </div>
+      <RangeContext.Provider value={contextValue}>
+        <SelectWeek />
+      </RangeContext.Provider>
       <Calendar
         mode="range"
         min={7}
@@ -89,3 +38,8 @@ function Calender() {
 }
 
 export default Calender
+
+interface initialValue {
+  range: DateRange | undefined
+}
+export const RangeContext = createContext<initialValue>({ range: undefined })
