c9209b588150b2725edcf8707cc4e3cbc45a6f71
LinQuixote <leein9404@naver.com>
2024-07-26 16:30:21 +0900

    fix: 소셜 로그인 유저데이터 업데이트 해결

--- src/app/api/auth/log-in/route.ts
+++ src/app/api/auth/log-in/route.ts
@@ -1,3 +1,40 @@
+// import { NextRequest, NextResponse } from "next/server"
+// import { createClient } from "@/supabase/server"
+
+// export async function POST(request: NextRequest) {
+//   const data = await request.json()
+//   const email = data.email as string
+//   const password = data.password as string
+//   const supabase = createClient()
+//   const {
+//     data: { user },
+//     error,
+//   } = await supabase.auth.signInWithPassword({
+//     email,
+//     password,
+//   })
+
+//   console.log("로그인 error :", error?.code, error?.name, error)
+//   if (error && error.message === 'Invalid login credentials') {
+//     // Get all property names of the error object
+//     let properties = Object.getOwnPropertyNames(error);
+
+//     // Create an array to store property-value pairs
+//     let propertyStrings = [];
+
+//     // Iterate over each property and build the output string
+//     properties.forEach(function (prop) {
+//       console.log(prop, error[prop]);  // Log each property and its value
+//       propertyStrings.push(prop + ': ' + error[prop]);
+//   });
+//   let alertMessage = propertyStrings.join('\n');
+//   }
+//   if (!user) {
+//     return NextResponse.json({ error: "Wrong information." }, { status: 401 })
+//   }
+//   return NextResponse.json(user)
+// }
+
 import { NextRequest, NextResponse } from "next/server"
 import { createClient } from "@/supabase/server"
 
@@ -8,6 +45,7 @@ export async function POST(request: NextRequest) {
   const supabase = createClient()
   const {
     data: { user },
+    error,
   } = await supabase.auth.signInWithPassword({
     email,
     password,
@@ -16,5 +54,6 @@ export async function POST(request: NextRequest) {
   if (!user) {
     return NextResponse.json({ error: "Wrong information." }, { status: 401 })
   }
+
   return NextResponse.json(user)
 }
