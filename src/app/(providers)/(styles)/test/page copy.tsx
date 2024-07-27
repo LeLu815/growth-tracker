a2700d0e1eef9dad50475d3e4408d2ce2cc85efb
minyoungKuk <75466519+minyoungKuk@users.noreply.github.com>
2024-07-27 17:25:19 +0900

    feat: swiper test

---
+++ src/app/(providers)/(styles)/test/page copy.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import ChallengeList from "./_components/ChallengeList"
+
+function MyChallengePage() {
+  return (
+    <div className="my-20 flex flex-col items-center">
+      <h1 className="text-center text-5xl font-black">내 챌린지</h1>
+      <ChallengeList />
+    </div>
+  )
+}
+
+export default MyChallengePage
