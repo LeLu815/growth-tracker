c0e04114242f78431ded966adfe3618531d5227d
minyoungKuk <75466519+minyoungKuk@users.noreply.github.com>
2024-07-27 22:17:31 +0900

    feat: swiper test

--- src/app/(providers)/(styles)/test/create/_components/Milestone/DragDropContainer.tsx
+++ src/app/(providers)/(styles)/test/create/_components/Milestone/DragDropContainer.tsx
@@ -16,9 +16,7 @@ import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
 
-import { Navigation, Pagination } from "swiper/modules"
-import { Swiper, SwiperSlide } from "swiper/react"
-
+import MilestoneSwiper from "../MilestoneSwiper"
 import MilestoneComponent from "./MilestoneComponent"
 
 type RoutineType = {
@@ -225,32 +223,8 @@ function DragDropContainer() {
 
       <div className="mt-5">
         <h2 className="mb-3 text-lg font-semibold">스와이프 마일스톤</h2>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={10}
-          navigation
-          pagination={{ clickable: true }}
-          modules={[Navigation, Pagination]}
-          className="w-[600px]"
-        >
-          {data.map((milestone) => (
-            <SwiperSlide key={milestone.id}>
-              <div className="mx-auto mb-4 flex w-[300px] flex-col gap-2 rounded-lg border border-gray-400 bg-gray-100 p-4">
-                <h3 className="text-md font-semibold">{milestone.name}</h3>
-                <ul className="flex h-full flex-col justify-between p-4">
-                  {milestone.routines.map((routine) => (
-                    <li
-                      className="flex cursor-grab items-center justify-between rounded-lg border border-slate-300 bg-green-200 p-8 shadow-md"
-                      key={routine.id}
-                    >
-                      {routine.content}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+
+        <MilestoneSwiper data={data} />
       </div>
     </div>
   )
