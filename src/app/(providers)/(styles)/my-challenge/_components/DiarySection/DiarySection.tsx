5c5a9763770abe13ab4f3e17872bc9547249d329
sobbingtofu <kukubasak@naver.com>
2024-07-29 00:25:19 +0900

    feat: 모든 체크박스가 선택되지 않아도 소감 입력할 수 있도록 변경

--- src/app/(providers)/(styles)/my-challenge/_components/DiarySection/DiarySection.tsx
+++ src/app/(providers)/(styles)/my-challenge/_components/DiarySection/DiarySection.tsx
@@ -16,26 +16,30 @@ function DiarySection({
     queryKey: ["fetchCurrentUserRoutineDoneDaily"],
   })
 
+  const [inputText, setInputText] = useState("")
+
   const targetMilestoneIndex = currentUserRoutineDoneDaily.findIndex((item) => {
     return item.milestone_id == milestoneId
   })
 
   const handleDiarySubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    console.log(event.target)
+    console.log(currentUserRoutineDoneDaily[targetMilestoneIndex].id)
+    console.log(inputText)
   }
 
   return (
     <div
       className={
-        !currentUserRoutineDoneDaily[targetMilestoneIndex]?.is_success
-          ? "hidden"
-          : ""
+        !currentUserRoutineDoneDaily[targetMilestoneIndex]?.is_success ? "" : ""
       }
     >
       <h5>오늘의 소감</h5>
       <form className="mt-2 flex flex-col gap-y-3" onSubmit={handleDiarySubmit}>
-        <textarea className="h-[150px] resize-none border-2 border-black px-2 py-2" />
+        <textarea
+          className="h-[150px] resize-none border-2 border-black px-2 py-2"
+          onChange={(event) => setInputText(event.target.value)}
+        />
         <button className="border-2 border-black px-2 py-2">저장하기</button>
       </form>
     </div>
