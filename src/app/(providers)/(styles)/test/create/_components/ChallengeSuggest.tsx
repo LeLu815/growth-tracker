0bce43ddde29c76baec98aea381b7c0f82a5be6f
LinQuixote <leein9404@naver.com>
2024-08-01 11:06:10 +0900

    feat: 달력 뺴고 기능 구현

--- src/app/(providers)/(styles)/test/create/_components/ChallengeSuggest.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react"
-
-interface ChallengeSuggest {}
-
-function ChallengeSuggest(props: PropsWithChildren<ChallengeSuggest>) {
-  return <div>{}</div>
-}
-
-export default ChallengeSuggest
