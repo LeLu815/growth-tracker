151bb36936fcc1beab422c6863c82d848ddc69a0
minyoungKuk <75466519+minyoungKuk@users.noreply.github.com>
2024-08-04 23:30:51 +0900

    style: 일단 한번 합치자..

--- types/challenge.ts
+++ types/challenge.ts
@@ -1,5 +1,14 @@
 import { Database } from "./supabase"
 
 export type PostType = Database["public"]["Tables"]["challenge"]["Row"] & {
-  user: { nickname: string }
+  user: { nickname: string; profile_image_url: string }
+  goal: string
+  like_cnt: number
+  template_cnt: number
+  state: string
+  category: string
+  progress: string
+  image: string
+  bookmarked: string[]
+  liked: string[]
 }
