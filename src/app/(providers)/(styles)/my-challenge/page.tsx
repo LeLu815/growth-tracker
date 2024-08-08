889b487db223d1463d7af75ddd1e7d8230e63049
sobbingtofu <kukubasak@naver.com>
2024-08-08 16:37:59 +0900

    feat: 이인님 무한스크롤 가져오기

--- src/app/(providers)/(styles)/my-challenge/page.tsx
+++ src/app/(providers)/(styles)/my-challenge/page.tsx
@@ -3,6 +3,7 @@ import Page from "@/components/Page"
 
 import ChallengeList from "./_components/ChallengeList"
 import DatePickerContainer from "./_components/DatePickerContainer"
+import InfiniteDateScroll from "./_components/InfiniteDateScroll"
 import MyChallengeNavBar from "./_components/MyChallengeNavBar"
 
 function MyChallengePage() {
@@ -12,6 +13,7 @@ function MyChallengePage() {
         <h1 className="mb-8 ml-2 text-[20px] font-bold">내 챌린지</h1>
         <div className="flex flex-col items-center">
           <MyChallengeNavBar />
+          <InfiniteDateScroll />
           <DatePickerContainer />
           <ChallengeList />
         </div>
