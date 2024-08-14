4519c9ae3b01a94f1895db7e7535ce606df64c64
minyoungKuk <75466519+minyoungKuk@users.noreply.github.com>
2024-08-14 16:09:12 +0900

    feat: page component 구조 잡기

--- src/app/(providers)/_components/Header/DefaultHeader.tsx
+++ src/app/(providers)/_components/Header/DefaultHeader.tsx
@@ -11,7 +11,12 @@ function DefaultHeader({
   className,
 }: PropsWithChildren<DefaultHeaderProps>) {
   return (
-    <BaseHeader className={classNames(className, "mx-auto max-w-[480px]")}>
+    <BaseHeader
+      className={classNames(
+        className,
+        "mx-auto min-w-[320px] max-w-[480px] sm:max-w-[480px] md:max-w-[768px] lg:max-w-[1024px]"
+      )}
+    >
       {children}
     </BaseHeader>
   )
