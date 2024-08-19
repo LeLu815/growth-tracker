ab1674896d3776185d7c7697884a1d7dcca034f6
LinQuixote <leein9404@naver.com>
2024-08-20 00:29:19 +0900

    feat: 데스크톱 브라우저 팝업 구현

--- src/components/Modal/CustomModal.tsx
+++ src/components/Modal/CustomModal.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import { useModal } from "@/context/modal.context"
+import { throttle } from "lodash"
 import { CSSTransition } from "react-transition-group"
 
+import CloseIcon02 from "../Icon/CloseIcon02"
 import BackDrop from "./BackDrop"
 
 interface ModalProps {
@@ -20,9 +22,20 @@ const CustomModal = ({
 }: ModalProps) => {
   const modal = useModal()
   const [isShow, setIsShow] = useState(false)
+  const [width, setWidth] = useState(window.innerWidth)
+  const handleResize = throttle(() => {
+    setWidth(window.innerWidth)
+  }, 300)
 
   useEffect(() => {
     setIsShow(true)
+  }, [])
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+    return () => {
+      // cleanup
+      window.removeEventListener("resize", handleResize)
+    }
   }, [])
 
   const handleCloseModal = () => {
@@ -30,13 +43,23 @@ const CustomModal = ({
     setTimeout(() => modal.close(), 300)
   }
 
-  const transitionClasses = {
-    enter: "translate-y-full",
-    enterActive: "translate-y-0 transition-transform duration-300 ease-in-out",
-    exit: "translate-y-0",
-    exitActive:
-      "translate-y-full transition-transform duration-300 ease-in-out",
-  }
+  const transitionClasses =
+    width >= 1024
+      ? {
+          enter: "opacity-0",
+          enterActive:
+            "opacity-100 transition-opacity duration-300 ease-in-out",
+          exit: "opacity-100",
+          exitActive: "opacity-0 transition-opacity duration-300 ease-in-out",
+        }
+      : {
+          enter: "translate-y-full",
+          enterActive:
+            "translate-y-0 transition-transform duration-300 ease-in-out",
+          exit: "translate-y-0",
+          exitActive:
+            "translate-y-full transition-transform duration-300 ease-in-out",
+        }
 
   return (
     <BackDrop>
@@ -46,15 +69,27 @@ const CustomModal = ({
         classNames={transitionClasses}
         unmountOnExit
       >
-        <div className="fixed bottom-0 left-1/2 h-[80vh] w-full min-w-[320px] -translate-x-1/2 translate-y-full transform overflow-auto rounded-t-[20px] bg-white px-4 transition sm:w-full">
-          <div className="sticky top-0 z-50 flex h-[50px] justify-center bg-gradient-to-t from-transparent via-white to-white pt-[10px]">
-            <div
-              onClick={() => handleCloseModal()}
-              className="mt-[5px] h-[5px] w-[120px] flex-shrink-0 cursor-pointer rounded-full bg-slate-300 opacity-90"
-            ></div>
+        {width >= 1024 ? (
+          <div className="fixed left-1/2 top-1/2 min-h-[650px] w-full max-w-[480px] -translate-x-1/2 -translate-y-1/2 transform overflow-hidden rounded-[20px] bg-white px-5 py-8">
+            <button
+              className="absolute right-[39px] top-[30px] z-50 cursor-pointer"
+              onClick={handleCloseModal}
+            >
+              <CloseIcon02 width={12} height={12} />
+            </button>
+            <div className="relative mx-auto max-w-[640px]">{children}</div>
           </div>
-          <div className="mx-auto max-w-[640px]">{children}</div>
-        </div>
+        ) : (
+          <div className="fixed bottom-0 left-1/2 h-[80vh] w-full min-w-[320px] -translate-x-1/2 translate-y-full transform overflow-auto rounded-t-[20px] bg-white px-4 transition sm:w-full">
+            <div className="sticky top-0 z-50 flex h-[50px] justify-center bg-gradient-to-t from-transparent via-white to-white pt-[10px]">
+              <div
+                onClick={() => handleCloseModal()}
+                className="mt-[5px] h-[5px] w-[120px] flex-shrink-0 cursor-pointer rounded-full bg-slate-300 opacity-90"
+              ></div>
+            </div>
+            <div className="mx-auto max-w-[640px]">{children}</div>
+          </div>
+        )}
       </CSSTransition>
     </BackDrop>
   )
