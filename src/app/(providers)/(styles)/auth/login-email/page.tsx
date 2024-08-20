481a688058a7c2fb7a7a9095dc89918329142289
minyoungKuk <75466519+minyoungKuk@users.noreply.github.com>
2024-08-20 11:05:12 +0900

    feat: metadata 추가

--- src/app/(providers)/(styles)/auth/login-email/page.tsx
+++ src/app/(providers)/(styles)/auth/login-email/page.tsx
@@ -1,7 +1,25 @@
+import { Metadata } from "next"
+
 import Box from "@/components/Box"
 import Page from "@/components/Page"
 
 import LoginEmailClient from "./_components/LoginEmailClient"
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "로그인 - 디딧",
+    description: "디딧에 로그인 하고 나의 챌린지를 등록해 보세요",
+    openGraph: {
+      title: "로그인 - 디딧",
+      description: "디딧에 로그인 하고 나의 챌린지를 등록해 보세요",
+      url: "https://growth-tracker-text.vercel.app/auth/login-email",
+    },
+    twitter: {
+      title: "로그인 - 디딧",
+      description: "디딧에 로그인 하고 나의 챌린지를 등록해 보세요",
+    },
+  }
+}
 
 function LoginEmailPage() {
   return (
--- src/app/(providers)/(styles)/my-page/layout.tsx
+++ src/app/(providers)/(styles)/my-page/layout.tsx
@@ -1,7 +1,24 @@
 import { PropsWithChildren } from "react"
+import { Metadata } from "next"
 
 import Page from "@/components/Page"
 import ResponsiveLayout from "@/app/(providers)/(styles)/my-page/_components/ResponsiveLayout"
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "마이페이지 - 디딧",
+    description: "마이 페이지에서 사용자 정보를 확인하고 설정할 수 있습니다.",
+    openGraph: {
+      title: "마이페이지 - 디딧",
+      description: "마이 페이지에서 사용자 정보를 확인하고 설정할 수 있습니다.",
+      url: "https://growth-tracker-text.vercel.app/my-page",
+    },
+    twitter: {
+      title: "마이페이지 - 디딧",
+      description: "마이 페이지에서 사용자 정보를 확인하고 설정할 수 있습니다.",
+    },
+  }
+}
 
 function MyPageLayout({ children }: PropsWithChildren) {
   return (
